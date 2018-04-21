ec2b52b0e570a7a670f2f6c3810bdc0b02668745
Nilay <justnilay17@gmail.com>
2018-04-21 22:53:13 +0530

    New Miners.rkt

--- miners.rkt
+++ miners.rkt
@@ -1,17 +1,40 @@
 #lang racket
 (require "project.rkt")
-(require pfds/heap/binomial)
+(require data/heap)
+(require racket/class)
 
-(define struct event (process time))
+(struct event (process time) #:transparent)
+
+(define no_of_zeros 6)
 
 (define global_events%
   (class object%
-    (define events-list (heap (lambda (x y) (< (event-time x) (event-time y))))) 
-    (init-field global_timer)
-    (define  
+    (init-field (events-list (make-heap (lambda (x y) (< (event-time x) (event-time y)))))) 
+    (init-field (global_timer 0))
+    
+    (super-new)
 
+    (define/public (insert_event process1 time1)
+      (begin
+        (define event-new (event process1 time1))
+        (heap-add! events-list event-new)))
+        
+    (define/public (process)
+      (cond
+        [(= (heap-count events-list) 0) (begin
+                                          (set! global_timer (+ 1 global_timer))
+                                          (continue-mining 1 no_of_zeros minerlist)
+                                          (process))]
+        [else (begin
+               (define event2 (heap-min events-list))
+               (heap-remove-min! events-list)
+               (if (< global_timer (event-time event2))
+                   (begin
+                     (continue-mining (- (event-time event2) no_of_zeros global_timer))
+                     ((event-process event2)))
+                   ((event-process event2))))]))))
 
-
+(define globaleventslist (make-object global_events%))
 
 
 (define node%
@@ -20,65 +43,95 @@
     (init initial-connections)
     (define connections initial-connections)
     (define private-key)
-    (define received_messages '())
     (init-field localtime 0)
+    
     (define local_Blockchain initial_Blockchain)
-    (define Forks (make-vector 10)) 
-    (init initial_Blockchain)
-    (define temp_Blockchain)
     
-    
-    (super-new)
+    (define total-accepted-payments '())
+    (define received-payments-to-be-used '())
+    (define pending-payments-to-confirm '())
+    (define pending-payments-to-be-made '())
 
+    (define Forks (make-vector 3))    
+    (init initial_Blockchain)
+
+    (super-new)
+    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     (define/public (add-new-connection node1 link-delay1)
       (set! connections (cons (list node1 link-delay1) connections)))
-
     (define/public (delete-connection node1)
       (set! connections (filter (lambda (x) (not (= (car x) node1))) connections)))
-
     (define/public (edit-connection node1 link_delay_new)
       (set! connections (map (lambda (x)
                                (if (equal? (car x) node1)
                                    (list node1 link_delay_new)
                                    (list (car x) (cadr x))))
                              connections)))
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     (define/public (broadcast message time)
       (define (send-msg conn msg time)
-        (cond [(null? conn) (display "Message Broadcasted")]
+        (cond [(null? conn) (display "Message Broadcasted: time:")
+                            (display (send globaleventslist global-timer))]
               [(list? conn) (begin
-                              (send (car (car conn)) receive (cadr (car conn)) msg time)
+                              (define (f) (send (car (car conn)) receive msg (+ (cadr (car conn)) time)))
+                              (send globaleventslist insert_event f (+ (cadr (car conn)) time))
                               (send-msg (cdr conn) msg))]
               [else (display "Connections of invalid type")]))
       (send-msg connections message time))
 
-    (define/public (receive message)
-      (cond [(Block? message) (cond [(Extension? local_Blockchain message) (Add_new_fork Forks message)]
-                                    [(Extension_Fork? message) (begin
-                                                                 (Update_Forks Forks message)
-                                                                 (if (Safe_Fork_Available? Forks)
-                                                                     (Update Block_chain)
-                                                                     (void)))]
+    (define/public (receive message time)
+      (cond [(Block? message) (cond [(Extension? local_Blockchain message) (set! Forks (Add_new_fork Forks message))]
+                                    [(Extension_Fork? Fork message) (begin
+                                                                     (set! Forks (Update_Forks Forks message))
+                                                                     (if
+                                                                      (Safe_Fork_Available? Forks)
+                                                                      (begin
+                                                                        (set! local_Blockchain (Update local_Blockchain))
+                                                                        (Accept-New-Payments time)
+                                                                        (Make-New-Payments time)
+                                                                        )
+                                                                      (void)))]
                                     [else (display "Block rejected by: ")
                                           (display nodename)])]
             [else (void)]))
+
+  (define/public (attempt_transaction nodename2 amount2 time)
+    (let* [(trans_new (Make-Payment nodename2 amount2))]
+      (cond [(trans? trans_new) (broadcast trans_new time)]
+            [else (set! pending-payments-to-be-made (cons (nodename2 amount time) pending-payments-to-be-made))])))                                  
+
+))
+    
+    (define miner%
+      (class object%
+        (init pubkey)
+        (define publickey pubkey)
+
+        (define pending-transactions-block '()))
         
-      
+        (super-new)
 
-    
-      
-    
+        (define/public (get-publickey)
+        (publickey))
 
-
-
-
-
-(define miner%
-  (class object%
-    (init pubkey)
-    (define publickey pubkey)
-    (super-new)
-    (define/public (get-publickey)
-      (publickey))
+        (define/override (receive message time)
+          (cond [(Block? message) (cond [(Extension? local_Blockchain message) (set! Forks (Add_new_fork Forks message))]
+                                        [(Extension_Fork? Fork message) (begin
+                                                                          (set! Forks (Update_Forks Forks message))
+                                                                          (if
+                                                                           (Safe_Fork_Available? Forks)
+                                                                           (begin
+                                                                             (set! local_Blockchain (Update local_Blockchain))
+                                                                             (Accept-New-Payments time)
+                                                                             (Make-New-Payments time)
+                                                                             )
+                                                                           (void)))]
+                                        [else (display "Block rejected by: ")
+                                              (display nodename)])]
+                [(trans? message) (if (belongs-pending-list? message)
+                                      (void)
+                                      (set! received-payments-to-be-used (cons message received-payments-to-be-used)))]
+                                   ))
     ))
     
